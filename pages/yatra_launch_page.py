e98af584090dddadbdc5b98daa3e2d3c1e7e56cf
Yuri <yuri.barru@gmail.com>
2023-05-08 13:53:40 +0700

    create pom and seperate from tc

---
+++ pages/yatra_launch_page.py
@@ -0,0 +1,58 @@
+import time
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class LaunchPage():
+    def __init__(self, driver, wait):
+        self.driver = driver
+        self.wait = wait
+
+    def departFrom(self, departLocation):
+        depart_from = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_city']")))
+        depart_from.click()
+        depart_from.send_keys(departLocation)
+        time.sleep(5)
+        depart_from.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+    def goingTo(self, goingToLocation):
+        going_to = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_arrival_city']")))
+        going_to.click()
+        time.sleep(2)
+        going_to.send_keys(goingToLocation)
+        time.sleep(5)
+
+        search_results = self.wait.until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='viewport']//div[1]/li")))
+
+        for results in search_results:
+            if "New York (JFK)" in results.text:
+                results.click()
+                break
+
+        time.sleep(5)
+
+    def selectDate(self, departureDate):
+        self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_date']"))).click()
+
+        all_dates = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']"
+                                        ))).find_elements(By.XPATH,
+                                                          "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']")
+
+        for date in all_dates:
+            if date.get_attribute("data-date") == departureDate:
+                date.click()
+                break
+
+
+    def clickSearch(self):
+        time.sleep(4)
+        self.driver.find_element(By.XPATH, "//input[@value='Search Flights']").click()
+        time.sleep(4)
--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -4,50 +4,25 @@ import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from pages.yatra_launch_page import LaunchPage
 
 @pytest.mark.usefixtures("setup")
 class TestSearchAndVerifyFilter():
     def test_search_flights(self):
         # Launching browser and opening the travel website
 
+        lp = LaunchPage(self.driver, self.wait)
         # Provide going from location
-        depart_from = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_city']")))
-        depart_from.click()
-        depart_from.send_keys("New Delhi")
-        depart_from.send_keys(Keys.ENTER)
+        lp.departFrom("New Delhi")
 
         # Provide going to location
-        going_to = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_arrival_city']")))
-        going_to.click()
-        time.sleep(2)
-        going_to.send_keys("New York")
-        search_results = self.wait.until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='viewport']//div[1]/li")))
-
-        for results in search_results:
-            if "New York (JFK)" in results.text:
-                results.click()
-                break
-
-        # To resolve sync issues
-        self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_date']"))).click()
-
-        all_dates = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']"
-                ))).find_elements(By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']")
+        lp.goingTo("New York")
 
         # Select the departure date
-        for date in all_dates:
-            if date.get_attribute("data-date") == "23/08/2021":
-                date.click()
-                break
+        lp.selectDate("10/05/2023")
 
         # Click on flight search button
-        self.driver.find_element(By.XPATH, "//input[@value='Search Flights']").click()
-        time.sleep(4)
+        lp.clickSearch()
 
         pageLength = self.driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);var pageLength=document.body.scrollHeight;return pagelength;"
