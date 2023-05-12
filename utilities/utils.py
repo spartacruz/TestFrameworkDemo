cb4ee3c61652f1d29250440bd57f4c86a6974c72
Yuri <yuri.barru@gmail.com>
2023-05-12 11:19:44 +0700

    Change to assert to softest - soft_assert -- can continue to validate all elements and not break if there is a failed one

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,8 +1,17 @@
+import softest
 
-class Utilities:
+
+class Utilities(softest.TestCase):
     def assertListItemText(self, list, value):
 
         for stop in list:
             print("The text is: " + stop.text)
-            assert stop.text == value
-            print("assert pass")
+
+            self.soft_assert(self.assertEqual, stop.text, value)
+            # assert stop.text == value
+            # print("assert pass")
+
+            if stop.text == value:
+                print("Test Passed")
+            else:
+                print("Test Failed")
