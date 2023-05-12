33ff55a77724b4ee10f18831be54b9bd736894d7
Yuri <yuri.barru@gmail.com>
2023-05-12 10:55:35 +0700

    refactor - seprating object creation and become a default fixture

--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -6,18 +6,24 @@ from utilities.utils import Utilities
 
 @pytest.mark.usefixtures("setup")
 class TestSearchAndVerifyFilter():
+
+    @pytest.fixture(autouse=True)
+    def class_setup(self):
+        self.lp = LaunchPage(self.driver)
+        self.ut = Utilities()
+
     def test_search_flights(self):
         # Launching browser and opening the travel website
 
         # Provide going from location
-        lp = LaunchPage(self.driver)
-        sf = lp.searchFlight("New Delhi", "New York", "15/05/2023")
+        # lp = LaunchPage(self.driver)
+        sf = self.lp.searchFlight("New Delhi", "New York", "15/05/2023")
 
         # Click on flight search button
-        lp.clickSearch()
+        self.lp.clickSearch()
 
         #scrolling element
-        lp.page_scroll()
+        self.lp.page_scroll()
 
         # Select the filter 1 stop
         sf.filter_flights_by_stop("1 Stop")
@@ -26,5 +32,5 @@ class TestSearchAndVerifyFilter():
         allstops1 = sf.get_search_flight_results()
         print(len(allstops1))
 
-        ut = Utilities()
-        ut.assertListItemText(allstops1, "1 Stop")
+        # ut = Utilities()
+        self.ut.assertListItemText(allstops1, "1 Stop")
