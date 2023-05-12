62c1e142152eac24dbafab348bc5de6c50c78d19
Yuri <yuri.barru@gmail.com>
2023-05-12 10:59:19 +0700

    simulate - create a new test case with different param

--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -12,25 +12,26 @@ class TestSearchAndVerifyFilter():
         self.lp = LaunchPage(self.driver)
         self.ut = Utilities()
 
-    def test_search_flights(self):
+    def test_search_flights_1stop(self):
         # Launching browser and opening the travel website
-
         # Provide going from location
-        # lp = LaunchPage(self.driver)
         sf = self.lp.searchFlight("New Delhi", "New York", "15/05/2023")
-
         # Click on flight search button
         self.lp.clickSearch()
-
         #scrolling element
         self.lp.page_scroll()
-
         # Select the filter 1 stop
         sf.filter_flights_by_stop("1 Stop")
-
         # Verify that the filtered results show flights having only 1 stop
         allstops1 = sf.get_search_flight_results()
         print(len(allstops1))
-
-        # ut = Utilities()
         self.ut.assertListItemText(allstops1, "1 Stop")
+
+    def test_search_flights_2stop(self):
+        sf = self.lp.searchFlight("New Delhi", "New York", "15/05/2023")
+        self.lp.clickSearch()
+        self.lp.page_scroll()
+        sf.filter_flights_by_stop("2 Stop")
+        allstops1 = sf.get_search_flight_results()
+        print(len(allstops1))
+        self.ut.assertListItemText(allstops1, "2 Stop")
