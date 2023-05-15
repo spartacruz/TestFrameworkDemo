84b53a62a63404996831174133c4103025503593
Yuri <yuri.barru@gmail.com>
2023-05-15 10:02:06 +0700

    adding ActionChains class

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -3,14 +3,16 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
+from selenium.webdriver.common.action_chains import ActionChains
 
 @pytest.fixture(scope="class")
 def setup(request):
     options = Options()
-    options.add_argument(r"--no-sandbox")  # Bypass OS security model
-    options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
+    options.add_argument("no-sandbox")  # Bypass OS security model
+    options.add_argument("disable-notifications")
+    # options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
     # options.add_argument(r"--disable-infobars")  # disabling infobars
-    options.add_argument("disable-extensions")  # disabling extensions
+    # options.add_argument("disable-extensions")  # disabling extensions
     # options.add_argument(r"--disable-gpu")  # applicable to windows os only
     # options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
     # options.add_argument(r"--user-data-dir=C:\Users\speci\AppData\Local\Google\Chrome\User Data")
@@ -34,11 +36,15 @@ def setup(request):
             fix_hairline=True,
             )
 
+    # create action chain object
+    action = ActionChains(driver)
+
     driver.get("https://www.yatra.com/")
 
     # When a class that called this fixture, both of this var will be returned
     # so, the var object can be used within the test class (self)
     request.cls.driver = driver
+    request.cls.action = action
 
     #yeild works like tear down. Will be executed after all process / testcase done
     yield
