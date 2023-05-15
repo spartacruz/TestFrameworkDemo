bec4276aba52324ae8dcdeed318e51da6dd0a580
Yuri <yuri.barru@gmail.com>
2023-05-15 10:50:04 +0700

    add parameterize --browser and --url when run pytest command from cli

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -4,42 +4,43 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
 from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 
 @pytest.fixture(scope="class")
-def setup(request):
-    options = Options()
-    options.add_argument("no-sandbox")  # Bypass OS security model
-    options.add_argument("disable-notifications")
-    # options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
-    # options.add_argument(r"--disable-infobars")  # disabling infobars
-    # options.add_argument("disable-extensions")  # disabling extensions
-    # options.add_argument(r"--disable-gpu")  # applicable to windows os only
-    # options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-    # options.add_argument(r"--user-data-dir=C:\Users\speci\AppData\Local\Google\Chrome\User Data")
-    # options.add_argument(r'--profile-directory=Profile 2')  # e.g. Profile 3
-    options.add_argument("start-maximized")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
+def setup(request, browser, options, url):
+    if browser == "chrome":
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+
+        # Selenium Stealth settings
+        stealth(driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,
+                )
+
+    elif browser == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser == "edge":
+        driver = webdriver.Edge(executable_path=EdgeChromiumDriverManager().install())
+    else:
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
 
-    # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-    # driver = webdriver.Chrome(executable_path="C:/Users/speci/Downloads/chromedriver_win32 (2)/chromedriver.exe", chrome_options=options)
-
-    # Selenium Stealth settings
-    stealth(driver,
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            )
 
     # create action chain object
     action = ActionChains(driver)
 
-    driver.get("https://www.yatra.com/")
+    # driver.get("https://www.yatra.com/")
+    if url == None:
+        #default url
+        url = "https://www.yatra.com/"
+
+    driver.get(url)
 
     # When a class that called this fixture, both of this var will be returned
     # so, the var object can be used within the test class (self)
@@ -52,3 +53,28 @@ def setup(request):
     driver.close()
     driver.quit()
 
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--url")
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture(scope="class", autouse=True)
+def url(request):
+    return request.config.getoption("--url")
+
+@pytest.fixture(scope="class", autouse=True)
+def options(request):
+    options = Options()
+    options.add_argument("no-sandbox")  # Bypass OS security model
+    options.add_argument("disable-notifications")
+    # options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
+    # options.add_argument(r"--disable-infobars")  # disabling infobars
+    # options.add_argument("disable-extensions")  # disabling extensions
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    return options
