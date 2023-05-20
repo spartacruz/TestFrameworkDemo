8ad2a98b841a25824b41f26903a8deff4aba4472
Yuri <yuri.barru@gmail.com>
2023-05-20 16:54:51 +0700

    add read data from excel (helper) to utils

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,4 +1,5 @@
 import softest
+from openpyxl import Workbook, load_workbook
 
 
 class Utilities(softest.TestCase):
@@ -21,3 +22,18 @@ class Utilities(softest.TestCase):
                 print("Test Passed")
             else:
                 print("Test Failed")
+
+    def read_data_from_excel(filename, sheet):
+        datalist = []
+        wb = load_workbook(filename=filename)
+        sh = wb[sheet]
+        row_ct = sh.max_row
+        col_ct = sh.max_column
+
+        for i in range(2, row_ct + 1):
+            row = []
+            for j in range(1, col_ct + 1):
+                row.append(sh.cell(row=i, column=j).value)
+
+            datalist.append(row)
+        return datalist
