922a2f4a0d26c7916668d668ee13faa498902df0
Yuri <yuri.barru@gmail.com>
2023-05-20 21:08:48 +0700

    Need to fix -- add fuctionality on conftest to generate image screenshot on error validation

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,3 +1,5 @@
+import os.path
+import time
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,8 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-
-@pytest.fixture(scope="class")
+@pytest.fixture(autouse=True)
 def setup(request, browser, options, url):
     if browser == "chrome":
         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
@@ -77,3 +78,29 @@ def options(request):
     options.add_experimental_option('useAutomationExtension', False)
 
     return options
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        #always add url to report
+        extra.append(pytest_html.extras.url("http://www.google.com"))
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            #only add additional html or failure
+            report_directory = os.path.dirname(item.config.option.htmlpath)
+            file_name = str(int(round(time.time()*1000))) + ".png"
+            # file_name = report.nodeid.replace("::", "_") + ".png"
+            destinationFile = os.path.join(report_directory, file_name)
+            driver.save_screenshot(destinationFile)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:300px;height=200px"'\
+                    'onclick="window.open(this.src)" align="right"/></div>'%file_name
+                extra.append(pytest_html.extras.html(html))
+            report.extra = extra
+
+def pytest_html_report_title(report):
+    report.title = "Automation Report"
